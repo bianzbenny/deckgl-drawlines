07bdf993b1ae015e4d925721f6b9a47052dcd27f
bianzbenny <bianzbenny@users.noreply.github.com>
2020-05-21 20:44:59 -0700

    Update App.js
    add 2d 3d toggle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import DeckGL, { OrbitView } from "deck.gl";
+import DeckGL, { OrbitView, OrthographicView } from "deck.gl";
 
 import renderLayers from "./LineLayers";
 
 const resolution = 10;
+
 export default () => {
   const requestRef = React.useRef();
   const update = React.useRef();
@@ -22,12 +23,22 @@ export default () => {
     rotationX: 90,
     zoom: -1
   });
-
+  const views2d = new OrthographicView({ id: "2d-scene" });
+  const views3d = new OrbitView({
+    id: "3d-scene",
+    orbitAxis: "Z",
+    rotationX: 60
+  });
+  const [v2d, setV2d] = useState(true);
+  const click2d = () => {
+    setV2d(v2d ? false : true);
+  };
   return (
     <>
       <div id="maps">
         <DeckGL
-          views={new OrbitView()}
+          //views={}
+          views={v2d ? views2d : views3d}
           initialViewState={viewport}
           controller={true}
           layers={renderLayers({
@@ -36,6 +47,9 @@ export default () => {
             viewport: viewport
           })}
         />
+      </div>
+      <div id="ui">
+        <button onClick={click2d}> {v2d ? "2D" : "3D"}</button>
       </div>
     </>
   );
